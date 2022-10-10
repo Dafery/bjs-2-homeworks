133490b521833b2d019862d782e393b933159843
Maria Abramovich <mashkala2@gmail.com>
2022-10-10 15:46:08 +0300

    changed task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,48 @@
-function Student(name, gender, age) {
-    // Ваш код
+'use strict';
 
+const Student1 = new Student('Oleg', 'male', 25);
+const Student2 = new Student('Sofi', 'female', 27);
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) { 
+    this.marks = [mark]; 
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if(this.marks === undefined) { 
+    this.marks = [...mark]; 
+  } else {
+    this.marks.push(...mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  const Average = this.marks.reduce((acc, el, idx) => {
+    acc += el;
+
+    if (idx === this.marks.length - 1) {
+      return acc / this.marks.length;
+    }
+
+    return acc;
+  }, 0);
+
+  return Average;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject, this.marks;
+  this.excluded = reason;
+}
