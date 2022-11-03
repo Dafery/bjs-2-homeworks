333fb3145bafebf312b0af049527dae7eba53a65
Maria Abramovich <mashkala2@gmail.com>
2022-11-03 20:19:00 +0300

    changed task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,44 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = {};
+    
+  return function(...args) {
+    const hash = args.join(',');
+
+    if (hash in cache) {
+      return `Из кэша: ${cache[hash]}`;
+    }
+
+    const result = func(...args);
+    cache[hash] = result;    
+    
+    if (Object.keys(cache).length > 5) {
+      delete cache[Object.keys(cache)[0]];
+    }
+   
+    return `Вычисляем: ${result}`;
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+ 
+  function wrapper(...args) {
+    wrapper.allCount++;    
+    clearTimeout(timeoutId);
+    
+    if (timeoutId === null) {
+      wrapper.count++;
+      func(...args);
+    }
+    
+    timeoutId = setTimeout(() => {      
+      wrapper.count++;
+      func(...args);
+    }, delay);
+  }
+  
+  return wrapper;
 }
